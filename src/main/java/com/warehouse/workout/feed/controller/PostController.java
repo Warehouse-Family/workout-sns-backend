f4298ac2d5fe753c66c4e387f446409c643fae43
92SooJong <hsz3855@gmail.com>
2022-05-30 23:42:45 +0900

    화면에서 파일 수신가능하도록 DTO 변경

--- src/main/java/com/warehouse/workout/feed/controller/PostController.java
+++ src/main/java/com/warehouse/workout/feed/controller/PostController.java
@@ -4,10 +4,8 @@ import com.warehouse.workout.feed.dto.PostCreationRequestDto;
 import com.warehouse.workout.feed.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -22,11 +20,11 @@ public class PostController {
     final PostService postService;
 
     // 게시글 쓰기
-    @PostMapping("/api/v1/post")
-    public void createPost(@RequestParam("files") MultipartFile[] files,@RequestBody PostCreationRequestDto postCreationRequestDto){
-        log.info(files.toString());
+    @PostMapping(value = "/api/v1/post")
+    public void createPost(PostCreationRequestDto post ){
 
-        postService.createPost(postCreationRequestDto);
+
+        postService.createPost(post);
     }
 
 
--- src/main/java/com/warehouse/workout/feed/dto/PostCreationRequestDto.java
+++ src/main/java/com/warehouse/workout/feed/dto/PostCreationRequestDto.java
@@ -2,16 +2,18 @@ package com.warehouse.workout.feed.dto;
 
 import com.warehouse.workout.feed.entity.PostEntity;
 import lombok.Data;
+import lombok.ToString;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class PostCreationRequestDto {
 
     private String writer;
     private String content;
-    // 위치
+    private List<MultipartFile> files;
 
     public PostEntity toPostEntity(){
         return PostEntity.builder()
